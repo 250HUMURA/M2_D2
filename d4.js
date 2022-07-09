31529252c90cf966285cb3298c4727e2d86eeef4
kennedy <kennedymupenzih@outlook.com>
2022-07-09 23:34:14 +0200

    Create d4.js

---
+++ d4.js
@@ -0,0 +1,60 @@
+
+
+/* EXERCISE 1
+ Enumerate and describe the main datatypes in JavaScript. Try to explain the concepts as you were speaking to a 12 years old.
+*/
+
+Data type it is what kind of data that can be store and manipulate with java script 
+ 
+    jaascript data types include:-Number Data type
+                                 -String  Data type 
+                                 -BooleanData type 
+                                 -undefined Data type
+                                 -null Data type 
+                                 -Object Data type 
+
+/* EXERCISE 2
+ Try to describe what an object is, in your own words.
+*/
+
+Object is on eof data type, that store different types of data.
+
+/* EXERCISE 3
+ Write the code to execute an addition (a sum) of the numbers 12 and 20.
+*/
+
+console.log(12 + 20)
+
+/* EXERCISE 4
+ Create a variable named x and assign to it the number 12.
+*/
+
+let x = 12
+
+/* EXERCISE 5
+ Create a variable called name and assign to it your name as a string.
+*/
+
+let name = "Humura"
+/* EXERCISE 6
+ Execute a subtraction between the number 4 and the variable x you declared before (which is storing the value 12).
+*/
+
+console.log( 4 - x)
+
+/* EXERCISE 7
+Create two variables: name1 and name2. Assign to name1 the string "john", and to name2 the string "John" (this one has a capital J!)
+ Verify that name1 is different from name2 (HINT: check their equality to be false).
+ Finally, verify that the equality between name1 and name2 becomes true if both are lowercase (without changing the value of name2).
+ YOU DON'T NEED AN IF/ELSE BLOCK. It is enough to use console.log()
+*/
+
+let name1 = "john"
+let name2 = "John"
+
+console.log(name1 === name2)
+console.log(name1 !== name2)
+
+/* WHEN YOU ARE FINISHED
+ Upload the .js file on Eduflow before 5PM CET. In the next days we'll also learn how to use GIT!
+*/
